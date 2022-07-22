3041dfefe23d76970ef222e05cfee989cff721b0
Felipe Vergani <68627360+felipevergani@users.noreply.github.com>
2022-07-22 11:25:42 -0300

    Att 22/07/22

--- HeapSort/Heapsort.c
+++ HeapSort/Heapsort.c
@@ -6,31 +6,31 @@
 #define true 1
 //gcc -std=c99 Heapsort.c HeapsortMAIN.c -o heap.x
 
-void heapsort(int a[], int n) {
+void heapsort(int array[], int n) {
    int i = n / 2, pai, filho, t;
    while(true) {
       if (i > 0) {
           i--;
-          t = a[i];
+          t = array[i];
       } else {
           n--;
           if (n <= 0) return;
-          t = a[n];
-          a[n] = a[0];
+          t = array[n];
+          array[n] = array[0];
       }
       pai = i;
       filho = i * 2 + 1;
       while (filho < n) {
-          if ((filho + 1 < n)  &&  (a[filho + 1] > a[filho]))
+          if ((filho + 1 < n)  &&  (array[filho + 1] > array[filho]))
               filho++;
-          if (a[filho] > t) {
-             a[pai] = a[filho];
+          if (array[filho] > t) {
+             array[pai] = array[filho];
              pai = filho;
              filho = pai * 2 + 1;
           } else {
              break;
           }
       }
-      a[pai] = t;
+      array[pai] = t;
    }
 }
